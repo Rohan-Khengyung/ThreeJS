b1166f5f1dd44865ae177b007b77a72eefb25172
Rohan Khengyung <kazkozan38@gmail.com>
2025-11-30 18:17:32 +0545

    vite-react-uses

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,6 +1,6 @@
 import {Canvas, useFrame} from "@react-three/fiber";
 import { useRef } from "react";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Sparkles } from "@react-three/drei";
 
 const RotatingCube = () => {
   const meshRef = useRef();
@@ -15,6 +15,7 @@ const RotatingCube = () => {
     <mesh ref={meshRef}>
       <cylinderGeometry args={[1,1,1]} />
       <meshLambertMaterial color="#465585" emissive="#468585" />
+      <Sparkles count={100} scale={1} size={6} speed={0.002} noise={0.2} color={'orange'} />
     </mesh>
   )
 }
